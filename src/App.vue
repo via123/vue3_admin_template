1ac777e90dd0b729ccaf5f6118b2c79f8a9c2646
qianyun.1108 <lvqianyun>
2023-09-25 16:19:22 +0800

    feat: 配置初始化

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,8 @@
 -->
 <template>
   <div>
-    <h1>
-      我是App
-    </h1>
-
+    <h1>我是App</h1>
   </div>
 </template>
-<script setup lang="ts">
-</script>
-<style scoped>
-</style>
+<script setup lang="ts"></script>
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@
  * @FilePath: /project/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+});
